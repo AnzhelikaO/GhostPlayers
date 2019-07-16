673cc933883cf833518c26d62976c0bc5c8a596e
Anzhelika <qwerty5867192@gmail.com>
2019-07-16 22:33:54 +0900

    Add project files.

---
+++ GhostPlayers/GhostPlayersAPI.cs
@@ -0,0 +1,106 @@
+﻿#region Using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using TShockAPI;
+#endregion
+namespace GhostPlayers
+{
+    public static class GhostPlayersAPI
+    {
+        internal static bool[,] CanSee = new bool[Main.maxPlayers, Main.maxPlayers];
+        public static readonly byte[] All = Enumerable.Range(0, Main.maxPlayers)
+                                                      .Select(i => (byte)i)
+                                                      .ToArray();
+
+        #region CheckIfAPlayerCanSeeAnotherPlayer
+
+        public static bool CheckIfAPlayerCanSeeAnotherPlayer(byte Seer, byte Seen) =>
+            ((Seer == Seen) || (Seer == byte.MaxValue)
+            || (Seen == byte.MaxValue) || CanSee[Seer, Seen]);
+
+        #endregion
+        #region SetCanSee
+
+        public static void SetCanSee(byte Observer, bool CanSee, byte Target)
+        {
+            if ((Observer == Target)
+                    || (TShock.Players.ElementAtOrDefault(Observer)?.Active != true)
+                    || (TShock.Players.ElementAtOrDefault(Target)?.Active != true))
+                return;
+            if (CanSee)
+            {
+                GhostPlayersAPI.CanSee[Observer, Target] = true;
+
+                NetMessage.SendData(14, Observer, -1, null, Target, 1);
+                NetMessage.SendData(4, Observer, -1, null, Target);
+                NetMessage.SendData(13, Observer, -1, null, Target);
+                NetMessage.SendData(16, Observer, -1, null, Target);
+                NetMessage.SendData(30, Observer, -1, null, Target);
+                NetMessage.SendData(45, Observer, -1, null, Target);
+                NetMessage.SendData(42, Observer, -1, null, Target);
+                NetMessage.SendData(50, Observer, -1, null, Target);
+
+                Player plr = Main.player[Target];
+
+                for (int index = 0; index < 59; ++index)
+                    NetMessage.SendData(5, Observer, -1, null, Target, index,
+                        plr.inventory[index].prefix);
+
+                for (int index = 0; index < plr.armor.Length; ++index)
+                    NetMessage.SendData(5, Observer, -1, null, Target, 59 + index,
+                        plr.armor[index].prefix);
+
+                for (int index = 0; index < plr.dye.Length; ++index)
+                    NetMessage.SendData(5, Observer, -1, null, Target,
+                        59 + plr.armor.Length + index, plr.dye[index].prefix);
+
+                for (int index = 0; index < plr.miscEquips.Length; ++index)
+                    NetMessage.SendData(5, Observer, -1, null, Target,
+                        59 + plr.armor.Length + plr.dye.Length + index,
+                            plr.miscEquips[index].prefix);
+
+                for (int index = 0; index < plr.miscDyes.Length; ++index)
+                    NetMessage.SendData(5, Observer, -1, null, Target,
+                        59 + plr.armor.Length + plr.dye.Length
+                        + plr.miscEquips.Length + index, plr.miscDyes[index].prefix);
+            }
+            else
+            {
+                NetMessage.SendData(14, Observer, -1, null, Target, 0);
+                GhostPlayersAPI.CanSee[Observer, Target] = false;
+            }
+        }
+
+        public static void SetCanSee(byte Observer, bool CanSee, IEnumerable<byte> Targets)
+        {
+            if (Targets == null)
+                throw new ArgumentNullException(nameof(Targets));
+            foreach (byte target in Targets)
+                SetCanSee(Observer, CanSee, target);
+        }
+
+        public static void SetCanSee(IEnumerable<byte> Observers, bool CanSee, byte Target)
+        {
+            if (Observers == null)
+                throw new ArgumentNullException(nameof(Observers));
+            foreach (byte observer in Observers)
+                SetCanSee(observer, CanSee, Target);
+        }
+
+        public static void SetCanSee(IEnumerable<byte> Observers,
+            bool CanSee, IEnumerable<byte> Targets)
+        {
+            if (Observers == null)
+                throw new ArgumentNullException(nameof(Observers));
+            if (Targets == null)
+                throw new ArgumentNullException(nameof(Targets));
+            foreach (byte target in Targets)
+                foreach (byte observer in Observers)
+                    SetCanSee(observer, CanSee, target);
+        }
+
+        #endregion
+    }
+}
---
+++ GhostPlayers/GhostPlayersPlugin.cs
@@ -0,0 +1,91 @@
+﻿#region Using
+using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+#endregion
+namespace GhostPlayers
+{
+    [ApiVersion(2, 1)]
+    public class GhostPlayersPlugin : TerrariaPlugin
+    {
+        #region Description
+
+        public override string Name => "GhostPlayers";
+        public override string Author => "Anzhelika";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+        public override string Description => "Hides player from other players.";
+
+        #endregion
+        #region Constructor
+
+        public GhostPlayersPlugin(Main game)
+            : base(game)
+        {
+            for (int i = 0; i < Main.maxPlayers; i++)
+                for (int j = 0; j < Main.maxPlayers; j++)
+                    GhostPlayersAPI.CanSee[i, j] = true;
+        }
+
+        #endregion
+
+        #region Initialize, Dispose
+
+        public override void Initialize() =>
+            ServerApi.Hooks.NetSendBytes.Register(this, OnSendBytes, 10000);
+        
+        protected override void Dispose(bool Disposing)
+        {
+            if (Disposing)
+                ServerApi.Hooks.NetSendBytes.Deregister(this, OnSendBytes);
+            base.Dispose(Disposing);
+        }
+
+        #endregion
+
+        #region OnSendBytes
+
+        private void OnSendBytes(SendBytesEventArgs args)
+        {
+            byte seer = (byte)args.Socket.Id, seen;
+            int bOffset = args.Offset + 3;
+            switch (args.Buffer[args.Offset + 2])
+            {
+                case 4: case 5: case 12: case 13: case 14: case 16: case 30:
+                case 35: case 36: case 40: case 41: case 42: case 43: case 45:
+                case 50: case 51: case 55: case 58: case 62: case 66: case 80:
+                case 84: case 96: case 99: case 102: case 115: case 117: case 118:
+                    seen = args.Buffer[bOffset];
+                    break;
+                case 22: case 24: case 29: case 70:
+                    seen = args.Buffer[bOffset + 2];
+                    break;
+                case 20: case 61:
+                    seen = args.Buffer[bOffset];
+                    break;
+                case 27:
+                    seen = args.Buffer[bOffset + 24];
+                    break;
+                case 65:
+                    byte flag = args.Buffer[bOffset];
+                    if (((flag & 1) == 0) && ((flag & 4) == 0))
+                        return;
+                    seen = args.Buffer[bOffset + 1];
+                    break;
+                case 108:
+                    seen = args.Buffer[bOffset + 14];
+                    break;
+                case 110:
+                    seen = args.Buffer[bOffset + 4];
+                    break;
+                default:
+                    return;
+            }
+
+            if (!GhostPlayersAPI.CheckIfAPlayerCanSeeAnotherPlayer(seer, seen))
+                args.Handled = true;
+        }
+
+        #endregion
+    }
+}
